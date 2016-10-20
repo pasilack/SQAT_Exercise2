d1f93274ebb973937335115a0f5c5eb85737dc28
somename <someemail>
2016-10-20 15:00:40 +0300

    besouro automatic message

---
+++ src/CurrentPosition.java
@@ -0,0 +1,29 @@
+
+public class CurrentPosition {
+	private int x;
+	private int y;
+	private char heading;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public char getHeading() {
+		return heading;
+	}
+	public void setHeading(char heading) {
+		this.heading = heading;
+	}
+	
+	public CurrentPosition(int x, int y, char heading) {
+		
+	}
+}
