edcaf1eb0b300a27b11dfe91855d326871e32898
somename <someemail>
2016-10-20 15:17:12 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -40,6 +40,16 @@ public class TestPlanetExplorer {
 	}
 	
 	@Test
+	public void test_CommandTurnLeftFourTimesOnANewPlanet() {
+		PlanetExplorer pe = new PlanetExplorer(100, 100, "");
+		pe.executeCommand("l");
+		pe.executeCommand("l");
+		pe.executeCommand("l");
+		String result = pe.executeCommand("l");
+		assertEquals("0,0,N" , result);
+	}
+	
+	@Test
 	public void test_CommandTurnRightOnANewPlanet() {
 		PlanetExplorer pe = new PlanetExplorer(100, 100, "");
 		assertEquals("0,0,E" ,pe.executeCommand("r"));
