04314142595fb805b31b4ad3a4619ca7f46a22c0
somename <someemail>
2016-10-20 14:39:41 +0300

    besouro automatic message

--- src/Planet.java
+++ src/Planet.java
@@ -1,4 +1,24 @@
 
 public class Planet {
+	private int x;
+	private int y;
+	private int [][] grid;
+	
+	public Planet(int x, int y) {
+		this.x = x;
+		this.y = y;
+		grid = new int[x][y];
+	}
 
+	public int[][] getGrid() {
+		return grid;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
 }
